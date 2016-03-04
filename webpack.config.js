5d804bb049416e405bdc7e0adac5ffafd7db8be4
Munoz, Adan <adan.munoz@rakuten.com>
2016-03-04 11:30:55 +0900

    sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,17 +11,31 @@ module.exports = {
   
   resolve: {
     
-    root: [path.resolve("node_modules"), path.resolve("vendor")] ,
+    root: [
+      path.resolve("node_modules"),
+      path.resolve("vendor"),
+      path.resolve("src"),
+    ] ,
       
     extensions:
-      ["", ".webpack.js", ".web.js", ".js", ".jsx" ]
+      ["", ".webpack.js", ".web.js", ".js", ".jsx", ".scss", ".sass" ]
 
   },
 
   module: {
     loaders: [
-      {test: /\.jsx?$/, exclude: /node_modules/, loader: "babel",
-        query: {presets: ['react', 'es2015']}}
+      // Babel automatic loading
+      { test: /\.jsx?$/, 
+        exclude: /node_modules/, 
+        loader: "babel",
+        query: {presets: ['react', 'es2015']}
+      },
+     
+      // Sass automatic loading
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      }
     ]
     
   }
