d2e2e967769756f242981dc370c3cdc06efbf064
Helg, Luke <luke.helg@rakuten.com>
2016-09-13 17:09:51 +0900

    Remove unused imports

--- src/domain/store/reduce/users.js
+++ src/domain/store/reduce/users.js
@@ -15,14 +15,13 @@
 // @flow
 
 import { Map } from "immutable";
-import { map } from "lodash";
 import { getLogger } from "domain/logger";
 import type { State, User } from "domain/store/state/main";
 
 const logger = getLogger("Reduce/users");
 
 export function updateUsers(state : State, users: Array<User>) : State {
-  logger.debug(`Update users ${map(users, (user) => user.firstName)}`);
+  logger.debug(`Update users ${users.map((user) => user.firstName)}`);
   return new Map(state).set("users", users).toJS();
 }
 
